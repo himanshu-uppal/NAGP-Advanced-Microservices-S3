3583dc5ec48903e0f6a72555b52fee94208f30f9
Himanshu Uppal <himanshu.uppal@nagarro.com>
2020-04-01 14:13:16 +0530

    updated url variables into env variables

--- aggregate-service/routes/index.js
+++ aggregate-service/routes/index.js
@@ -5,8 +5,6 @@ const request = require('request');
 
 const router = Router();
 
-const SERVICE_URLS = require('../constants');
-
 router.get('/orderdetails/:id', async (req, res) => {
 
     try {
@@ -37,7 +35,7 @@ const getUserDetails = (id) => {
      // Return new promise
   return new Promise(function(resolve, reject) {
     // Do async job
-    let userUrl = `http://${SERVICE_URLS.USER_SERVICE_URL}/users/${id}`;
+    let userUrl = `http://${process.env.USER_SERVICE_URL}/users/${id}`;
     request.get(userUrl, function(err, resp, body) {
       if (err) {
         reject(err);
@@ -52,7 +50,7 @@ const getOrderDetails = (id) => {
     // Return new promise
  return new Promise(function(resolve, reject) {
    // Do async job
-   let orderUrl = `http://${SERVICE_URLS.ORDER_SERVICE_URL}/orders/${id}`;
+   let orderUrl = `http://${process.env.ORDER_SERVICE_URL}/orders/${id}`;
    request.get(orderUrl, function(err, resp, body) {
      if (err) {
        reject(err);
